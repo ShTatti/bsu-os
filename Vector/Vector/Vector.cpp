0026e75cfaabeb38d3e927bedd595ff2f12be665
ShTatti <tanushash04@gmail.com>
2022-09-15 09:49:22 +0300

    Initial commit

---
+++ Vector/Vector/Vector.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "Vector.h"
+#include "cmath"
+
+Vector::Vector() {
+	x.setNumber(0.0);
+	y.setNumber(0.0);
+}
+
+Vector::Vector(Number x1, Number y1) {
+	this->x.setNumber(x1.getNumber());
+	this->y.setNumber(y1.getNumber());
+}
+
+Number Vector::calculatePolarRadius() {
+	return Number(sqrt((x * x + y * y).getNumber()));
+}
+
+Number Vector::calculatePolarAngle() {
+	return Number(atan((y / x).getNumber()));
+}
+
+Vector Vector::operator+(const Vector& vector) {
+	return Vector(x + vector.x, y + vector.y);
+}
