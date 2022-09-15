09d6cb03eb26159db952c3fc680af77d55f05ccf
ShTatti <tanushash04@gmail.com>
2022-09-15 09:49:22 +0300

    Initial commit

---
+++ Lab1Part2/num/library.cpp
@@ -0,0 +1,42 @@
+#include "library.h"
+
+#include <iostream>
+
+Number::Number() {
+    number = 0;
+}
+
+Number::Number(double number) {
+    this->number = number;
+}
+
+double Number::getNumber() {
+    return number;
+}
+
+void Number::setNumber(double number) {
+    this->number = number;
+}
+
+Number Number::operator+(const Number& number) {
+    return Number(this->number + number.number);
+}
+
+Number Number::operator-(const Number& number) {
+    return Number(this->number - number.number);
+}
+
+Number Number::operator*(const Number& number) {
+    return Number(this->number * number.number);
+}
+
+Number Number::operator/(const Number& number) {
+    if (number.number == 0) {
+        throw "eror.division by zero";
+    }
+    return Number(this->number / number.number);
+}
+
+Number createNumber(double number) {
+    return Number(number);
+}
