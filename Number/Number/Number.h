ade6c5922b9f9cda9596c546f682bb32c6048304
ShTatti <tanushash04@gmail.com>
2022-09-15 09:49:22 +0300

    new comm
    done

---
+++ Number/Number/Number.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Number {
+private:
+	double number;
+
+public:
+	Number();
+	Number(double number);
+
+	double getNumber();
+	void setNumber(double number);
+
+	Number operator+(const Number& number);
+	Number operator-(const Number& number);
+	Number operator*(const Number& number);
+	Number operator/(const Number& number);
+};
+
+const Number NUMBER_ZERO = Number(0.0);
+const Number NUMBER_ONE = Number(1.0);
+
+Number createNumber(double number);
